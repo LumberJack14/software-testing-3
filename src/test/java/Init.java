db90a798ff343a310f8a39d59de1417e8be97539
user1 <pozdov.ivan@yandex.ru>
2025-06-02 23:14:19 +0300

    init

---
+++ src/test/java/Init.java
@@ -0,0 +1,29 @@
+import com.example.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import java.time.Duration;
+
+public class Init {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public final String BASE = "https://yandex.ru/maps/";
+    public String browser = "chrome"; // firefox
+
+    @BeforeEach
+    public void setUp() {
+        driver = DriverManager.initializeDriver(browser);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get(BASE);
+        System.out.println("Started test in browser: " + browser);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("Browser closed");
+        }
+    }
+}
