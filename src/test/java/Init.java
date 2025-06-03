040010c31e2da6b0e0d05fca806c9060ea39ca41
user1 <pozdov.ivan@yandex.ru>
2025-06-03 23:27:53 +0300

    proper driver usage

--- src/test/java/Init.java
+++ src/test/java/Init.java
@@ -1,10 +1,5 @@
 import com.example.DriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,26 +9,16 @@ public class Init {
     public WebDriver driver;
     public WebDriverWait wait;
     public final String BASE = "https://yandex.com/maps/";
-    public String browser = "chrome"; // firefox chrome
+
+    public String browser = "chrome"; // firefox
+    public boolean incognito = false;
 
     @BeforeEach
     public void setUp() {
-        if (browser.equalsIgnoreCase("firefox")) {
-            FirefoxProfile profile = new FirefoxProfile();
-            profile.setPreference("intl.accept_languages", "en-US");
-            FirefoxOptions options = new FirefoxOptions().setProfile(profile);
-            driver = new FirefoxDriver(options);
-        } else if (browser.equalsIgnoreCase("chrome")) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--lang=en-US");
-            driver = new ChromeDriver(options);
-        } else {
-            throw new IllegalArgumentException("Unsupported browser: " + browser);
-        }
-
+        driver = DriverManager.initializeDriver(browser, incognito);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get(BASE);
-        System.out.println("Started test in browser: " + browser);
+        System.out.println("Started test in browser: " + browser + " | Incognito: " + incognito);
     }
 
     @AfterEach
